34a57cf8eeacb731255440385c20f0c1d9b85c02
Ruii1112 <ruiiiii1112@gmail.com>
2022-05-20 18:58:46 +0900

    [add]ペジネーション

--- app/Post.php
+++ app/Post.php
@@ -10,4 +10,9 @@ class Post extends Model
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this -> orderBy('updated_at', 'DESC') -> limit($limit_count) -> get();
     }
+    
+    public function getPaginateByLimit(int $limit_count = 1){
+        // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this -> orderBy('updated_at', 'DESC') -> paginate($limit_count);
+    }
 }
