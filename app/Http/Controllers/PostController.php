e929162fe102f65f93f792e72c4adfa33967b4d9
Ruii1112 <ruiiiii1112@gmail.com>
2022-05-31 19:43:38 +0900

    [add] 質問機能

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,21 @@ class PostController extends Controller
     public function index(Post $post)
     {
         //Log::debug($post -> getPaginateByLimit());
-        return view('posts/index') -> with(['posts' => $post -> getPaginateByLimit()]);
+        $client = new \GuzzleHttp\Client();
+        $url = 'https://teratail.com/api/v1/questions';
+        
+        $response = $client -> request(
+            'GET',
+            $url,
+            ['Bearer' => config('services.teratail.token')]
+        );
+        
+        $questions = json_decode($response->getBody(), true);
+        
+        return view('posts/index') -> with([
+            'posts' => $post -> getPaginateByLimit(),
+            'questions' => $questions['questions'],
+        ]);
     }
     
     public function show(Post $post)
